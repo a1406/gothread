ce697868806085648410ecc5a939a0bb675c6eb9
jacktang <jack at zzfs>
2017-09-21 18:17:44 +0800

    1

--- hashtable/rcu_table/rcu_table.go
+++ hashtable/rcu_table/rcu_table.go
@@ -2,7 +2,7 @@ package rcu_table
 
 import (
 	"sync"
-	"sync/atomic"	
+//	"sync/atomic"	
 )
 
 //const bucket_num uint = 8
@@ -13,7 +13,7 @@ type Table_rcu struct {
 
 type ht_bucket struct {
 	head *ht_bucket_entry
-	readers int32
+//	readers int32
 	locker *sync.Mutex
 }
 
@@ -25,11 +25,11 @@ type ht_bucket_entry struct {
 func (t *Table_rcu)Num() uint {
 	var ret uint
 	for i := uint(0); i < t.size; i++ {
-		atomic.AddInt32(&t.ht_bkt[i].readers, 1)
+//		atomic.AddInt32(&t.ht_bkt[i].readers, 1)
 		for cur := t.ht_bkt[i].head; cur != nil; cur = cur.next {
 			ret++
 		}
-		atomic.AddInt32(&t.ht_bkt[i].readers, -1)		
+//		atomic.AddInt32(&t.ht_bkt[i].readers, -1)		
 	}
 	return ret
 }
@@ -43,36 +43,53 @@ func (t *Table_rcu)Init(size uint) {
 }
 func (t *Table_rcu)Lookup(k int) bool {
 	i := uint(k) % t.size
-	atomic.AddInt32(&t.ht_bkt[i].readers, 1)
+//	atomic.AddInt32(&t.ht_bkt[i].readers, 1)
 	for cur := t.ht_bkt[i].head; cur != nil; cur = cur.next {
 		if cur.data == k {
-			t.ht_bkt[i].locker.Unlock()
 			return true
 		}
 	}
-	atomic.AddInt32(&t.ht_bkt[i].readers, -1)		
+//	atomic.AddInt32(&t.ht_bkt[i].readers, -1)		
 	return false
 }
+
+func (t *ht_bucket_entry)copy_bucket() *ht_bucket_entry {
+	if t == nil {
+		return nil
+	}
+	
+	var ret ht_bucket_entry
+	pre := &ret
+	ret.data = t.data
+	
+	for cur := t.next; cur != nil; cur = cur.next {
+		var entry ht_bucket_entry
+		entry.data = cur.data
+		pre.next = &entry
+		pre = &entry
+	}
+	return &ret
+}
+
 func (t *Table_rcu)Insert(k int) {
 	i := uint(k) % t.size
 	var bucket ht_bucket_entry
 	bucket.data = k
 
 	t.ht_bkt[i].locker.Lock()
-	var new_head *ht_bucket_entry
-	new_head = t.ht_bkt[i].head
-	
+	new_head := t.ht_bkt[i].head.copy_bucket()
 	bucket.next = new_head
-	new_head = &bucket
-	
+	t.ht_bkt[i].head = &bucket
 	t.ht_bkt[i].locker.Unlock()		
 	return
 }
 func (t *Table_rcu)Delete(k int) bool {
 	i := uint(k) % t.size
 	
-	t.ht_bkt[i].locker.Lock()		
-	pre := t.ht_bkt[i].head
+	t.ht_bkt[i].locker.Lock()
+	new_head := t.ht_bkt[i].head.copy_bucket()
+	
+	pre := new_head
 	if pre == nil {
 		t.ht_bkt[i].locker.Unlock()				
 		return false
@@ -86,6 +103,7 @@ func (t *Table_rcu)Delete(k int) bool {
 	for cur := pre.next; cur != nil; cur = cur.next {
 		if cur.data == k {
 			pre.next = cur.next
+			t.ht_bkt[i].head = new_head
 			t.ht_bkt[i].locker.Unlock()					
 			return true
 		}
