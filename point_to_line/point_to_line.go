35eff487c0d8ec8e83fbeba0c65ea0b47ee5bd5f
jacktang <jack at zzfs>
2017-09-25 12:01:46 +0800

    1

--- point_to_line/point_to_line.go
+++ point_to_line/point_to_line.go
@@ -3,7 +3,18 @@ package main
 import (
 	"fmt"
 	"flag"
+	"math"
 )
+
+//直线(一般式):Ax+By+C=0坐标(Xo，Yo)，，那么这点到这直线的距离就为:(AXo+BYo+C)的绝对值除以根号下(A的平方加上B的平方
+//a=a b=-1 c=0
+func count_distance(a float64, x1, y1 float64) float64 {
+	//	(a * x1 - y1)
+	//	math.Sqrt(
+	t1 := math.Abs(a * x1 - y1)
+	t2 := math.Sqrt(a * a + 1)
+	return t1 / t2
+}
 
 //line: y = ax
 //point: x1, y1
@@ -16,5 +27,7 @@ func main() {
 	flag.Parse()
 	fmt.Printf("line: y = %dx\n", a)
 	fmt.Printf("point: [%.1f][%.1f]\n", x1, y1)
+	dist := count_distance(float64(a), x1, y1)
+	fmt.Printf("distance = %.3f\n", dist)
 }
 
